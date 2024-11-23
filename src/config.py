2ff260c854a983e35c1ac4f5c16591473f3446ea
danXignat <dan.ignat@student.unitbv.ro>
2024-11-23 14:06:00 +0200

    initial commit

---
+++ src/config.py
@@ -0,0 +1,3 @@
+LOGO_PATH = "../static/logo.png"
+TEMPLATES_PATH = '../data/template/'
+OUTPUT_PATH = "../data/output/"
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from frontend.MainWindow import MainWindow
+
+if __name__ == "__main__":
+    window = MainWindow()
+    
+    window.run()
+
